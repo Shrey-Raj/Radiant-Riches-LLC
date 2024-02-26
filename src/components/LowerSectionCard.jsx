8441b88c9a7b6b8bb4a856774d73041aededc121
Shrey Raj <shreyraj1653@gmail.com>
2024-02-26 16:51:09 +0530

    minor fixes

--- src/components/LowerSectionCard.jsx
+++ src/components/LowerSectionCard.jsx
@@ -1,7 +1,7 @@
 import { Flex, Tag, Typography, Image, Button } from "antd";
 import React from "react";
 
-const src= "src/assets/lowerSectionCardImg.svg" ; 
+const src= "src/imgs/lowerSectionCardImg.png" ; 
 
 const LowerSectionCard = () => {
   return (
